25a90795dc84ed8b24977b41932f69b9d788cd74
Dwayne Aquan <dwayneaquan@gmail.com>
2021-08-06 11:53:39 -0400

    removed login btn on Home

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,10 +4,12 @@
 <br>
     <div class="text-center">
         <h1>Welcome to PriceCheck!</h1>
-        <p>All your prices under one roof!</p>
-        <p><a class="btn btn-primary btn-lg px-4" href="/login" role="button">Login</a> 
-           <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
-
+        <h5>All your prices under one roof!</h5>
+        <br>
+        
+        <p>Register now and start uploading prices!</p>
+        <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
+        <br><br>
         <div class="row my-3 justify-content-center">
             <form action="/search" method="GET" class="col-sm-6">
                 <div class="input-group">
