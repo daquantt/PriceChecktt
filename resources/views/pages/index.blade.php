5ff6976bf03617c55355587e325060bdbb137a52
Dwayne Aquan <dwayneaquan@gmail.com>
2021-08-12 23:19:39 -0400

    reduced input field size

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,16 +4,15 @@
 <br>
     <div class="text-center">
         <h1>Welcome to PriceCheck!</h1>
-        <h5>All your prices under one roof!</h5>
+        <h5>All your prices under one roof!</h5>        
         <br>
         
         <h4>Register now and start uploading prices!</h4>
         <br>
-        <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
-    </div>    
-        
+        <a class="btn btn-success btn-lg px-5" href="/register" role="button">Register</a>
+
         <br><br>
-        <div class="row my-3 justify-content-center">
+        <div class="row mb-3 justify-content-center">
             <form action="/search" method="GET" class="col-sm-6">
                 <div class="input-group">
                     <input name="search" class="form-control" type="search" placeholder="Search Products" aria-label="Search">
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,44 +3,45 @@
 @section('content')
     <br>
     <h3>Create Post</h3><span>* required fields</span>
-    
-    {!! Form::open(['action' => 'App\Http\Controllers\PostsController@store', 'method' => 'POST']) !!}
         
-        <div class="input-group">
-            {{Form::label('date', 'Date of Price*', ['class' => 'input-group-text col-sm-2 mb-2 justify-content-end'])}}
-            {{Form::date('price_date', '', ['class' => 'form-control mb-2', 'placeholder' => 'Date of Price'])}}
-        </div>
-        <div class="input-group">
-            {{Form::label('title', 'Item*', ['class' => 'input-group-text col-sm-2 mb-2 justify-content-end'])}}
-            {{Form::text('title', '', ['class' => 'form-control mb-2', 'maxlength' => '30', 'placeholder' => 'Product'])}}
-        </div>
-        <div class="input-group")>
-            {{Form::label('price', 'Price*', ['class' => 'input-group-text col-sm-2 mb-2 justify-content-end'])}}
-            {{Form::text('price', '', ['class' => 'form-control mb-2 col col-sm-4', 'maxlength' => '12', 'placeholder' => 'Price'])}}
-        </div>
-        <div class="input-group">
-            {{Form::label('brand', 'Brand', ['class' => 'input-group-text col-sm-2 mb-2 justify-content-end'])}}
-            {{Form::text('brand', '', ['class' => 'form-control mb-2', 'maxlength' => '20', 'placeholder' => 'Brand'])}}
-        </div>
-        <div class="input-group">
-            {{Form::label('size', 'Size', ['class' => 'input-group-text col-sm-2 mb-2 justify-content-end'])}}
-            {{Form::text('size', '', ['class' => 'form-control mb-2', 'maxlength' => '8', 'placeholder' => 'Size'])}}
-        </div>
-        <div class="input-group">
-            {{Form::label('vendor', 'Vendor*', ['class' => 'input-group-text col-sm-2 mb-2 justify-content-end'])}}
-            {{Form::text('vendor', '', ['class' => 'form-control mb-2', 'maxlength' => '20', 'placeholder' => 'Vendor'])}}
-        </div>
-        <div class="input-group">
-            {{Form::label('location', 'Location', ['class' => 'input-group-text col-sm-2 mb-2 justify-content-end'])}}
-            {{Form::text('location', '', ['class' => 'form-control mb-2', 'maxlength' => '20', 'placeholder' => 'Location'])}}
-        </div>
-        <div class="input-group">
-            {{Form::label('comment', 'Comments', ['class' => 'input-group-text col-sm-2 mb-2 justify-content-end'])}}
-            {{Form::textarea('comment', '', ['class' => 'form-control mb-2', 'rows' => '1', 'maxlength' => '1000', 'placeholder' => 'Additional information'])}}
-        </div>
-        
-        
-        {{Form::submit('Submit', ['class' => 'btn btn-primary text-light'])}}
-        <a href="/dashboard" class="btn btn-secondary">Cancel</a>
-    {!! Form::close() !!}
+    <div class="col-md-6">
+        {!! Form::open(['action' => 'App\Http\Controllers\PostsController@store', 'method' => 'POST']) !!}
+            
+            <div class="input-group">
+                {{Form::label('date', 'Date of Price*', ['class' => 'input-group-text col-sm-3 mb-2'])}}
+                {{Form::date('price_date', '', ['class' => 'form-control mb-2', 'placeholder' => 'Date of Price'])}}
+            </div>
+            <div class="input-group">
+                {{Form::label('title', 'Item*', ['class' => 'input-group-text col-sm-3 mb-2'])}}
+                {{Form::text('title', '', ['class' => 'form-control mb-2', 'maxlength' => '30', 'placeholder' => 'Product'])}}
+            </div>
+            <div class="input-group")>
+                {{Form::label('price', 'Price*', ['class' => 'input-group-text col-sm-3 mb-2'])}}
+                {{Form::text('price', '', ['class' => 'form-control mb-2 col col-sm-4', 'maxlength' => '12', 'placeholder' => 'Price'])}}
+            </div>
+            <div class="input-group">
+                {{Form::label('brand', 'Brand', ['class' => 'input-group-text col-sm-3 mb-2'])}}
+                {{Form::text('brand', '', ['class' => 'form-control mb-2', 'maxlength' => '20', 'placeholder' => 'Brand'])}}
+            </div>
+            <div class="input-group">
+                {{Form::label('size', 'Size', ['class' => 'input-group-text col-sm-3 mb-2'])}}
+                {{Form::text('size', '', ['class' => 'form-control mb-2', 'maxlength' => '8', 'placeholder' => 'Size'])}}
+            </div>
+            <div class="input-group">
+                {{Form::label('vendor', 'Vendor*', ['class' => 'input-group-text col-sm-3 mb-2'])}}
+                {{Form::text('vendor', '', ['class' => 'form-control mb-2', 'maxlength' => '20', 'placeholder' => 'Vendor'])}}
+            </div>
+            <div class="input-group">
+                {{Form::label('location', 'Location', ['class' => 'input-group-text col-sm-3 mb-2'])}}
+                {{Form::text('location', '', ['class' => 'form-control mb-2', 'maxlength' => '20', 'placeholder' => 'Location'])}}
+            </div>
+            <div class="input-group">
+                {{Form::label('comment', 'Comments', ['class' => 'input-group-text col-sm-3 mb-2'])}}
+                {{Form::textarea('comment', '', ['class' => 'form-control mb-2', 'rows' => '1', 'maxlength' => '1000', 'placeholder' => 'Additional information'])}}
+            </div>            
+            
+            {{Form::submit('Upload', ['class' => 'btn btn-primary text-light'])}}
+            <a href="/dashboard" class="btn btn-secondary">Back</a>
+        {!! Form::close() !!}
+    </div>
 @endsection
